4c140b7abb354011f77a481bd3664c80541ede16
David P. Riedel <davidp.riedel@gmail.com>
2018-04-18 13:19:19 -0400

    new module for file filters

---
+++ src/EDGAR_XML_FileFilter.h
@@ -0,0 +1,49 @@
+// =====================================================================================
+//
+//       Filename:  EDGAR_XML_FileFilter.h
+//
+//    Description:  class which identifies EDGAR files which contain proper XML for extracting.
+//
+//        Version:  1.0
+//        Created:  04/18/2018 09:37:16 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:
+//
+// =====================================================================================
+
+
+	/* This file is part of ExtractEDGARData. */
+
+	/* ExtractEDGARData is free software: you can redistribute it and/or modify */
+	/* it under the terms of the GNU General Public License as published by */
+	/* the Free Software Foundation, either version 3 of the License, or */
+	/* (at your option) any later version. */
+
+	/* ExtractEDGARData is distributed in the hope that it will be useful, */
+	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+	/* GNU General Public License for more details. */
+
+	/* You should have received a copy of the GNU General Public License */
+	/* along with ExtractEDGARData.  If not, see <http://www.gnu.org/licenses/>. */
+
+// =====================================================================================
+//        Class:  EDGAR_XML_FileFilter
+//  Description:  class which EDGAR files to extract data from.
+// =====================================================================================
+
+#ifndef  _EDGAR_XML_FILTER_
+#define  _EDGAR_XML_FILTER_
+
+#include <experimental/string_view>
+
+bool UseEDGAR_File(std::string_view file_content)
+{
+    return false;
+}
+
+#endif
