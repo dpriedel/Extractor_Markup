35fd8e0e9428e298229799dda77a494420c04181
David P. Riedel <davidp.riedel@gmail.com>
2018-12-13 14:34:25 -0500

    Add class to provide range support for HTML content.

---
+++ src/HTML_FromFile.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  HTML_FromFile.cpp
+ *
+ *    Description:  class to make working with HTML content in EDGAR files
+ *                  fit in better with STL and maybe ranges
+ *
+ *        Version:  1.0
+ *        Created:  12/13/2018 02:31:12 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David P. Riedel (), driedel@cox.net
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "HTML_FromFile.h"
+
+/*
+ *--------------------------------------------------------------------------------------
+ *       Class:  HTML_FromFile
+ *      Method:  HTML_FromFile
+ * Description:  constructor
+ *--------------------------------------------------------------------------------------
+ */
+
+
+HTML_FromFile::HTML_FromFile ()
+{
+}  /* -----  end of method HTML_FromFile::HTML_FromFile  (constructor)  ----- */
+
+
---
+++ src/HTML_FromFile.h
@@ -0,0 +1,53 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  HTML_FromFile.h
+ *
+ *    Description:  class to make working with HTML content in EDGAR files
+ *                  fit in better with STL and maybe ranges
+ *
+ *        Version:  1.0
+ *        Created:  12/13/2018 02:26:57 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David P. Riedel (), driedel@cox.net
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  _HTML_FROMFILE_INC_
+#define  _HTML_FROMFILE_INC_
+
+/*
+ * =====================================================================================
+ *        Class:  HTML_FromFile
+ *  Description:  make HTML content available via iterators.
+ * =====================================================================================
+ */
+class HTML_FromFile
+{
+    public:
+        /* ====================  LIFECYCLE     ======================================= */
+        HTML_FromFile ();                             /* constructor */
+
+        /* ====================  ACCESSORS     ======================================= */
+
+        /* ====================  MUTATORS      ======================================= */
+
+        /* ====================  OPERATORS     ======================================= */
+
+    protected:
+        /* ====================  METHODS       ======================================= */
+
+        /* ====================  DATA MEMBERS  ======================================= */
+
+    private:
+        /* ====================  METHODS       ======================================= */
+
+        /* ====================  DATA MEMBERS  ======================================= */
+
+}; /* -----  end of class HTML_FromFile  ----- */
+
+#endif   /* ----- #ifndef HTML_FromFile_INC  ----- */
