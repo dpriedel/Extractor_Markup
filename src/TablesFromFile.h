fec6956dc0d2f9c9f1dd2e1c31396cde0e5f5980
David P. Riedel <davidp.riedel@gmail.com>
2019-09-09 09:02:03 -0400

    reorganized header code
    - move iterator definition out from container
    - add comment blocks

--- src/TablesFromFile.h
+++ src/TablesFromFile.h
@@ -63,65 +63,7 @@ class TablesFromHTML
 {
 public:
 
-    class table_itor: public std::iterator<
-                    std::forward_iterator_tag,      // iterator_category
-                    TableData,                     // value_type
-                    ptrdiff_t,
-                    TableData const *,
-                    TableData const &
-                    >
-    {
-        const boost::regex regex_table_{R"***(<table.*?>.*?</table>)***",
-            boost::regex_constants::normal | boost::regex_constants::icase};
-        boost::cregex_token_iterator doc_;
-        boost::cregex_token_iterator end_;
-
-        // used to clean up the data
-
-        const boost::regex regex_bogus_em_dash{R"***(&#151;)***"};
-        const boost::regex regex_real_em_dash{R"***(&#8212;)***"};
-        const boost::regex regex_hi_ascii{R"***([^\x00-\x7f])***"};
-        const boost::regex regex_multiple_spaces{R"***( {2,})***"};
-        const boost::regex regex_space_tab{R"***( \t)***"};
-        const boost::regex regex_tab_before_paren{R"***(\t+\))***"};
-        const boost::regex regex_tabs_spaces{R"***(\t[ \t]+)***"};
-        const boost::regex regex_dollar_tab{R"***(\$\t)***"};
-        const boost::regex regex_leading_tab{R"***(^\t)***"};
-
-        const std::string pseudo_em_dash = "---";
-        const std::string delete_this = "";
-        const std::string one_space = " ";
-        const std::string one_tab = R"***(\t)***";
-        const std::string just_paren = ")";
-        const std::string just_dollar = "$";
-
-
-        EM::sv html_;
-        
-        TableData table_data_;
-
-        std::string CollectTableContent(EM::sv html);
-        std::string ExtractTextDataFromTable (CNode& a_table);
-        std::string FilterFoundHTML (const std::string& new_row_data);
-
-    public:
-
-        table_itor() = default;
-        explicit table_itor(EM::sv html);
-
-        table_itor& operator++();
-        const table_itor operator++(int) { table_itor retval = *this; ++(*this); return retval; }
-
-        bool operator==(const table_itor& other) const { return doc_ == other.doc_; }
-        bool operator!=(const table_itor& other) const { return !(*this == other); }
-
-        reference operator*() const { return table_data_; };
-        pointer operator->() const { return &table_data_; }
-
-        EM::sv to_sview() const { return table_data_.current_table_html_; }
-        bool TableHasMarkup (EM::sv table);
-    };
-
+    class table_itor;
     using iterator = table_itor;
     using const_iterator = table_itor;
 
@@ -153,5 +95,87 @@ private:
     EM::sv html_;
 
 }; /* -----  end of class TablesFromHTML  ----- */
+
+
+// =====================================================================================
+//        Class:  TablesFromHTML::table_itor
+//  Description:  iterator from contents of TablesFromHTML container.
+// =====================================================================================
+//
+class TablesFromHTML::table_itor: public std::iterator<
+                std::forward_iterator_tag,      // iterator_category
+                TableData,                     // value_type
+                ptrdiff_t,
+                TableData const *,
+                TableData const &
+                >
+{
+public:
+    // ====================  LIFECYCLE     ======================================= 
+
+    table_itor() = default;
+    explicit table_itor(EM::sv html);
+
+    // ====================  ACCESSORS     ======================================= 
+
+    EM::sv to_sview() const { return table_data_.current_table_html_; }
+    bool TableHasMarkup (EM::sv table);
+
+    // ====================  MUTATORS      ======================================= 
+
+    table_itor& operator++();
+    table_itor operator++(int) { table_itor retval = *this; ++(*this); return retval; }
+
+    // ====================  OPERATORS     ======================================= 
+
+    bool operator==(const table_itor& other) const { return doc_ == other.doc_; }
+    bool operator!=(const table_itor& other) const { return !(*this == other); }
+
+    reference operator*() const { return table_data_; };
+    pointer operator->() const { return &table_data_; }
+
+protected:
+    // ====================  METHODS       ======================================= 
+
+    // ====================  DATA MEMBERS  ======================================= 
+
+private:
+    // ====================  METHODS       ======================================= 
+
+    std::string CollectTableContent(EM::sv html);
+    std::string ExtractTextDataFromTable (CNode& a_table);
+    std::string FilterFoundHTML (const std::string& new_row_data);
+
+    // ====================  DATA MEMBERS  ======================================= 
+
+    const boost::regex regex_table_{R"***(<table.*?>.*?</table>)***",
+        boost::regex_constants::normal | boost::regex_constants::icase};
+    boost::cregex_token_iterator doc_;
+    boost::cregex_token_iterator end_;
+
+    // used to clean up the data
+
+    const boost::regex regex_bogus_em_dash{R"***(&#151;)***"};
+    const boost::regex regex_real_em_dash{R"***(&#8212;)***"};
+    const boost::regex regex_hi_ascii{R"***([^\x00-\x7f])***"};
+    const boost::regex regex_multiple_spaces{R"***( {2,})***"};
+    const boost::regex regex_space_tab{R"***( \t)***"};
+    const boost::regex regex_tab_before_paren{R"***(\t+\))***"};
+    const boost::regex regex_tabs_spaces{R"***(\t[ \t]+)***"};
+    const boost::regex regex_dollar_tab{R"***(\$\t)***"};
+    const boost::regex regex_leading_tab{R"***(^\t)***"};
+
+    const std::string pseudo_em_dash = "---";
+    const std::string delete_this = "";
+    const std::string one_space = " ";
+    const std::string one_tab = R"***(\t)***";
+    const std::string just_paren = ")";
+    const std::string just_dollar = "$";
+
+
+    EM::sv html_;
+    
+    TableData table_data_;
+}; // -----  end of class TablesFromHTML::table_itor  ----- 
 
 #endif   /* ----- #ifndef TABLESFROMFILE_INC  ----- */
