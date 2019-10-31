385b07ff6aec13c8dba14eedb86b999ce824a527
David P. Riedel <davidp.riedel@gmail.com>
2019-10-31 11:57:44 -0400

    some cleanups
    - consistent use of the same number regex
    - don't search whole document...just first part

--- src/SharesOutstanding.cpp
+++ src/SharesOutstanding.cpp
@@ -16,6 +16,7 @@
 
 #include "SharesOutstanding.h"
 
+#include <algorithm>
 #include <charconv>
 #include <functional>
 
@@ -30,8 +31,9 @@
 //--------------------------------------------------------------------------------------
 SharesOutstanding::SharesOutstanding ()
 {
-    const std::string s02{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) and \b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b shares issued and outstanding(?:,)? (?:at|as of))***"};
+    const std::string s02{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) and (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares issued and outstanding(?:,)? (?:at|as of))***"};
     const std::string s03{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of common stock.{0,30}? (?:at|as of))***"};
+    const std::string s04{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of.{1,30}? common stock.{0,30}?outstanding (?:at|as of))***"};
     const std::string s05{R"***(common stock .{0,50}? \b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b and (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares issued and outstanding, respectively)***"};
     const std::string s06{R"***(common stock .{0,70}? \b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b and (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares issued and outstanding at.{1,30}?, respectively)***"};
     const std::string s07{R"***((?:issuer|registrant) had (?:outstanding )?(\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of (?:its )?common stock(?:,.{0,30}?,)?(?: outstanding)?)***"};
@@ -39,15 +41,16 @@ SharesOutstanding::SharesOutstanding ()
     const std::string s10{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) (?:shares issued and outstanding|issued and outstanding shares))***"};
     const std::string s30{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) .?number of shares of common stock(?:, .*?,)? outstanding)***"};
     const std::string s34{R"***(there were (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of .{0,30}?common stock.{0,30}? outstanding)***"};
-    const std::string s35{R"***(there were (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) outstanding shares of the issuer s common stock.{0,30}? on)***"};
+    const std::string s35{R"***(there were (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) outstanding shares of the issuer.s common stock.{0,30}? on)***"};
     const std::string s37{R"***((?:at|as of).{1,20}? there were (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares outstanding of common stock)***"};
 //    const std::string s40{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of the (?:registrant.s|issuer.s) common stock(?:, .*?,)? (?:were )?outstanding)***"};
     const std::string s40{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of the (?:registrant.s|issuer.s) common stock(?:, .*?,)? (?:were )?outstanding)***"};
     const std::string s41{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of common stock of the (?:registrant|issuer) were outstanding as of)***"};
     const std::string s42{R"***((?:as of .{0,30?})?(\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares of the (?:registrant.s|issuer.s) common stock issued and outstanding(?: as of)?)***"};
-    const std::string s50{R"***(authorized, .*? [0-9,]{5,} issued and (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) outstanding)***"};
+    const std::string s50{R"***(authorized,.*?\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b issued and (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) outstanding)***"};
     const std::string s60{R"***((?:registrant.s|issuer.s) shares of common stock outstanding was (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) as of)***"};
-    const std::string s62{R"***(shares (?:issued and )?outstanding of the registrant s common stock as of .{1,20}? was.{0,20}? (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares)***"};
+    const std::string s62{R"***(shares (?:issued and )?outstanding of the registrant.s common stock as of .{1,20}? was.{0,20}? (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b))***"};
+    const std::string s64{R"***(shares of the (?:registrant.s|issuer.s) common stock outstanding as of .{1,20}? was.{0,20}? (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b))***"};
     const std::string s70{R"***(common stock .{0,30}? \b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b shares authorized issued (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) shares)***"};
     const std::string s72{R"***(common stock .authorized \b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b shares .{1,30}? issued (\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b))***"};
     const std::string s80{R"***((\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b) common stock(?:s)? issued and outstanding as of)***"};
@@ -72,6 +75,7 @@ SharesOutstanding::SharesOutstanding ()
     shares_matchers_.emplace_back("r40", std::make_unique<boost::regex const>(s40, my_flags));
     shares_matchers_.emplace_back("r41", std::make_unique<boost::regex const>(s41, my_flags));
     shares_matchers_.emplace_back("r60", std::make_unique<boost::regex const>(s60, my_flags));
+    shares_matchers_.emplace_back("r64", std::make_unique<boost::regex const>(s64, my_flags));
     shares_matchers_.emplace_back("r62", std::make_unique<boost::regex const>(s62, my_flags));
     shares_matchers_.emplace_back("r50", std::make_unique<boost::regex const>(s50, my_flags));
     shares_matchers_.emplace_back("r70", std::make_unique<boost::regex const>(s70, my_flags));
@@ -79,7 +83,7 @@ SharesOutstanding::SharesOutstanding ()
     shares_matchers_.emplace_back("r06", std::make_unique<boost::regex const>(s06, my_flags));
     shares_matchers_.emplace_back("r05", std::make_unique<boost::regex const>(s05, my_flags));
     shares_matchers_.emplace_back("r02", std::make_unique<boost::regex const>(s02, my_flags));
-    shares_matchers_.emplace_back("r09", std::make_unique<boost::regex const>(s09, my_flags));
+    shares_matchers_.emplace_back("r04", std::make_unique<boost::regex const>(s04, my_flags));
     shares_matchers_.emplace_back("r34", std::make_unique<boost::regex const>(s34, my_flags));
     shares_matchers_.emplace_back("r37", std::make_unique<boost::regex const>(s37, my_flags));
     shares_matchers_.emplace_back("r35", std::make_unique<boost::regex const>(s35, my_flags));
@@ -92,6 +96,7 @@ SharesOutstanding::SharesOutstanding ()
     shares_matchers_.emplace_back("r03", std::make_unique<boost::regex const>(s03, my_flags));
     shares_matchers_.emplace_back("r88", std::make_unique<boost::regex const>(s88, my_flags));
     shares_matchers_.emplace_back("r89", std::make_unique<boost::regex const>(s89, my_flags));
+    shares_matchers_.emplace_back("r09", std::make_unique<boost::regex const>(s09, my_flags));
     shares_matchers_.emplace_back("r90", std::make_unique<boost::regex const>(s90, my_flags));
     shares_matchers_.emplace_back("r91", std::make_unique<boost::regex const>(s91, my_flags));
 }  // -----  end of method SharesOutstanding::SharesOutstanding  (constructor)  ----- 
@@ -102,7 +107,8 @@ int64_t SharesOutstanding::operator() (EM::sv html) const
     std::unique_ptr<GumboOutput, std::function<void(GumboOutput*)>> output(gumbo_parse_with_options(&options, html.data(), html.length()),
             [&options](GumboOutput* output){ gumbo_destroy_output(&options, output); });
 
-    std::string the_text = CleanText(output->root);
+    std::string the_text_whole = CleanText(output->root);
+    std::string the_text = the_text_whole.substr(0, 100000);
 
     gumbo_destroy_output(&options, output.release());
 
@@ -146,7 +152,7 @@ std::string SharesOutstanding::CleanText(GumboNode* node) const
 {
     //    this code is based on example code in Gumbo Parser project
 
-    const boost::regex regex_nbr{R"***(\b[0-9,]{5,}\b)***"};
+    const boost::regex regex_nbr{R"***(\b[1-9](?:[0-9]{0,2})(?:,[0-9]{3})+\b)***"};
     const boost::regex regex_hi_ascii{R"***([^\x00-\x7f])***"};
     const boost::regex regex_multiple_spaces{R"***( {2,})***"};
     const boost::regex regex_nl{R"***(\n{1,})***"};
