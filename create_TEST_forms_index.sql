881042cc255faad1aa4e4495ea622ee2d55a6c4d
David P. Riedel <davidp.riedel@gmail.com>
2025-08-28 13:31:15 -0400

    feature: build separate index table
    - start with a clone of the root table from the sec forms data base

---
+++ create_TEST_forms_index.sql
@@ -0,0 +1,37 @@
+-- this will create tables and related structures for
+-- the SEC database which is extracted from the files
+-- downloaded from the SEC FTP server.
+
+-- see link below for hints
+-- https://wiki.postgresql.org/wiki/Don%27t_Do_This
+
+CREATE SCHEMA IF NOT EXISTS test_forms_index AUTHORIZATION extractor_pg;
+
+DROP TABLE IF EXISTS test_forms_index.sec_form_index CASCADE;
+
+CREATE TABLE test_forms_index.sec_form_index
+(
+    filing_id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE,
+    cik TEXT NOT NULL,
+    alternate_id1 TEXT DEFAULT NULL,
+    alternate_id2 TEXT DEFAULT NULL,
+    company_name TEXT NOT NULL,
+    file_name TEXT DEFAULT NULL,
+    amended_file_name TEXT DEFAULT NULL,
+    symbol TEXT DEFAULT NULL,
+    sic TEXT NOT NULL,
+    form_type TEXT NOT NULL,
+    date_filed DATE DEFAULT NULL,
+    amended_date_filed DATE DEFAULT NULL,
+    period_ending DATE NOT NULL,
+    period_context_id TEXT DEFAULT NULL,
+    shares_outstanding NUMERIC DEFAULT -1,
+    data_source TEXT NOT NULL,
+    UNIQUE (cik, form_type, period_ending),
+    PRIMARY KEY (cik, form_type, period_ending)
+);
+
+ALTER TABLE test_forms_index.sec_form_index OWNER TO extractor_pg;
+
+DROP INDEX IF EXISTS idx_period_ending;
+CREATE INDEX idx_period_ending ON test_forms_index.sec_form_index (period_ending);
