0af838d82221ad6b1f9521a099699366d13f1fca
David P. Riedel <davidp.riedel@gmail.com>
2025-09-17 16:01:29 -0400

    chore: add console logger to start with
    - also, formatting

--- Main.cpp
+++ Main.cpp
@@ -15,59 +15,63 @@
 //
 // =====================================================================================
 
-	/* This file is part of CollectEDGARData. */
+/* This file is part of CollectEDGARData. */
 
-	/* CollectEDGARData is free software: you can redistribute it and/or modify */
-	/* it under the terms of the GNU General Public License as published by */
-	/* the Free Software Foundation, either version 3 of the License, or */
-	/* (at your option) any later version. */
+/* CollectEDGARData is free software: you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation, either version 3 of the License, or */
+/* (at your option) any later version. */
 
-	/* CollectEDGARData is distributed in the hope that it will be useful, */
-	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
-	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
-	/* GNU General Public License for more details. */
+/* CollectEDGARData is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+/* GNU General Public License for more details. */
 
-	/* You should have received a copy of the GNU General Public License */
-	/* along with CollectEDGARData.  If not, see <http://www.gnu.org/licenses/>. */
+/* You should have received a copy of the GNU General Public License */
+/* along with CollectEDGARData.  If not, see <http://www.gnu.org/licenses/>. */
 
-
-#include <signal.h>
 #include <iostream>
- 
+#include <signal.h>
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+
 #include "ExtractorApp.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	//	help to optimize c++ stream I/O (may screw up threaded I/O though)
+    //	help to optimize c++ stream I/O (may screw up threaded I/O though)
 
-	std::ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
 
     // getting disconnected from DB can terminate the program
     // so, let's avoid it.  (apparently, this is an OK strategy)
     //
     signal(SIGPIPE, SIG_IGN);
 
-	int result = 0;
-	try
-	{
-		ExtractorApp myApp(argc, argv);
-		bool startup_ok = myApp.Startup();
+    auto my_default_logger = spdlog::stdout_color_mt("ExtractorApp_logger");
+    spdlog::set_default_logger(my_default_logger);
+
+    int result = 0;
+    try
+    {
+        ExtractorApp myApp(argc, argv);
+        bool startup_ok = myApp.Startup();
         if (startup_ok)
         {
             myApp.Run();
             myApp.Shutdown();
         }
-	}
+    }
 
-    catch (std::system_error& e)
+    catch (std::system_error &e)
     {
         // any system problems, we eventually abort, but only after finishing work in process.
 
         auto ec = e.code();
-        std::cerr << "Category: " << ec.category().name() << ". Value: " << ec.value() <<
-                ". Message: " << ec.message() << '\n';
+        std::cerr << "Category: " << ec.category().name() << ". Value: " << ec.value() << ". Message: " << ec.message()
+                  << '\n';
     }
-    catch (std::exception& e)
+    catch (std::exception &e)
     {
         std::cout << "Problem collecting files: " << e.what() << '\n';
     }
@@ -76,5 +80,5 @@ int main(int argc, char** argv)
         std::cout << "Unknown problem collecting files." << '\n';
     }
 
-	return result;
+    return result;
 }
